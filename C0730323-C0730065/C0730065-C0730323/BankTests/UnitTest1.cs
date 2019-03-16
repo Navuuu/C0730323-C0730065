17573b297bc6f6f96633e5fef50d0558d00d9129
visualstudio <visualstudio@122-PC10>
2019-03-15 20:30:46 -0400

    finally testing

---
+++ C0730323-C0730065/C0730065-C0730323/BankTests/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { }
+            public void Debit_WithValidAmount_UpdatesBalance()
+
+            {
+
+                //Arrange
+
+                double beginningBalance = 11.99;
+
+                double debitAmount = 4.55;
+
+                double expected = 7.44;
+
+                BankAccount account = new BankAccount("Parminder kaur and ravneet kaur", beginningBalance);
+
+
+
+                //Act
+
+                account.Debit(debitAmount);
+
+
+
+                //Assert
+
+                double actual = account.Balance;
+
+                Assert.AreEqual(expected, actual, 0.0001, "Account nt debited correctly");
+
+            }
+
+
+        }
+    }
